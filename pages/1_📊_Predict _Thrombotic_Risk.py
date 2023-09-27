7c61d7d31058ff43650a11872f8f93c9f9e710e8
Andrea Chavez Munoz <chavezmunoz.a@northeastern.edu>
2023-09-27 09:52:34 -0400

    Website layout created

---
+++ pages/1_📊_Predict _Thrombotic_Risk.py
@@ -0,0 +1,19 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title="Predict Thrombotic Risk",
+    page_icon="📊",
+)
+
+# Main layout
+st.title("Page title")
+st.markdown("""This page should be where users can input patient 
+            data and receive predictions regarding thrombotic risk. 
+            Users can input patient-specific information, 
+            and the model will generate a risk assessment based on the input data. 
+            This page should have a user-friendly interface for data input 
+            and result display.""")
+
+
+# Side bar layout
+st.sidebar.file_uploader("Upload file X")
