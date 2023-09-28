3302e9b4cc382526eea06777d6caec3e82ece70c
Faith Toner <toner.f@northeastern.edu>
2023-09-28 17:20:01 -0400

    paramter initial state

--- pages/2_🧠_Train_Model.py
+++ pages/2_🧠_Train_Model.py
@@ -1,4 +1,5 @@
 import streamlit as st
+import pandas as pd
 
 st.set_page_config(
     page_title="Train Model",
@@ -12,6 +13,15 @@ st.markdown("""This page is for administrators who want to retrain the model
             It can provide options for uploading new data, 
             selecting model algorithms, and adjusting training settings. """)
 
+# Radio button widget
+st.subheader("Selection of Highly Correlated Paramters")
+#percentages should turn into variables pulled from the model
+radio_pivpa = st.radio("Choose one parameter", ["Platlet Inhibition (75%)", "Platelet Aggregation (76%)"])
+
+#Below is an example idk what would be correlated
+radio_gvs=st.radio("Chose one parameter", ["Gender (50%)","Smoking (85%)"])
+
+st.write(f"You selected: {radio_pivpa} and {radio_gvs}")
 
 # Side bar layout
-st.sidebar.file_uploader("Upload file X")
+st.sidebar.file_uploader("Upload Data Set")
