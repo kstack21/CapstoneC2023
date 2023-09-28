55f9836761a7a8912a2110987355eb1dbdf5c128
Andrea Chavez Munoz <chavezmunoz.a@northeastern.edu>
2023-09-28 17:12:31 -0400

    Prediction piechart created

--- pages/2_🧠_Train_Model.py
+++ pages/2_🧠_Train_Model.py
@@ -1,4 +1,8 @@
 import streamlit as st
+import pandas as pd
+import os
+import plotly.express as px
+
 
 st.set_page_config(
     page_title="Train Model",
@@ -12,6 +16,21 @@ st.markdown("""This page is for administrators who want to retrain the model
             It can provide options for uploading new data, 
             selecting model algorithms, and adjusting training settings. """)
 
+#PREDICTION
+# Get data from folder
+path_pages = os.path.dirname(__file__)
+path_pages = path_pages.replace("/pages", "")
+data_path = os.path.join(path_pages, "data", "DummyResult.xlsx")
+
+# Get 10 most influencial elements
+prediction = pd.read_excel(data_path)
+prediction = prediction.T.squeeze()
+largest_contributor = prediction.nlargest(n=10, keep='first')
+largest_contributor = pd.DataFrame({'Category': largest_contributor.index, 'Value': largest_contributor.values})
+
+# Plot pie chart
+fig = px.pie(largest_contributor, names='Category', values='Value', title='Parameters contribution to risk')
+st.plotly_chart(fig, use_container_width=True)
 
 # Side bar layout
 st.sidebar.file_uploader("Upload file X")
