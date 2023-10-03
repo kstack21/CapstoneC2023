f908d0dc8b03533418f4e2fa8e5bc1588098be10
Andrea Chavez Munoz <chavezmunoz.a@northeastern.edu>
2023-10-02 22:10:48 -0400

    Data demographics graph added

--- pages/2_🧠_Train_Model.py
+++ pages/2_🧠_Train_Model.py
@@ -2,6 +2,8 @@ import streamlit as st
 import pandas as pd
 import os
 import plotly.express as px
+from plotly.subplots import make_subplots
+import plotly.graph_objects as go
 
 #--------------------------Helper functions--------------------------#
 def path_back_to(new_folder_name):
@@ -53,7 +55,31 @@ st.write("Total number of patients: ", len(df))
 st.write("Average patient age: ", df['Age'].mean())
 st.write("Percent of white patients: ", (df[df.White == 1].shape[0])/len(df) * 100, "%")
 
-st.bar_chart(df, x = "White")
+# Make pie subplots
+fig = make_subplots(2, 2, specs=[[{'type':'domain'}, {'type':'domain'}],[{'type':'xy'}, {'type':'xy'}]],
+                    subplot_titles=['Gender Distribution', 'Ethnicity Distribution','Age', 'BMI'])
+
+# Count binary values in the "Male" column
+male_counts = df['Male'].value_counts()
+male_labels = ['Male' if male_counts.index[0] else 'Female', 'Male' if not male_counts.index[0] else 'Female']
+# Create a pie chart for "Male"
+fig.add_trace(go.Pie(labels=male_labels, values=male_counts), row=1, col=1)
+
+# Count binary values in the "White" column
+white_counts = df['White'].value_counts()
+white_labels = ['White' if white_counts.index[0] else 'Non-White', 'White' if not white_counts.index[0] else 'Non-White']
+# Create a pie chart for "White"
+fig.add_trace(go.Pie(labels=white_labels, values=white_counts), row=1, col=2)
+
+# Age histogram
+fig.add_trace(go.Histogram(x=df["Age"], name="Age"), row=2, col=1)
+
+# BMI histogram
+fig.add_trace(go.Histogram(x=df["BMI"], name="BMI"), row=2, col=2)
+
+# Display histrograms
+st.plotly_chart(fig, use_container_width=True)
+
 #--------------------------Parameters--------------------------#
 # Radio button widget
 st.subheader("Selection of Highly Correlated Paramters")
