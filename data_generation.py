e629b265b1d58c9de50db5487a4937572841e7f1
Andrea Chavez Munoz <chavezmunoz.a@northeastern.edu>
2023-10-08 13:24:14 -0400

    Dummy data update. Thrm events to 20%

--- data_generation.py
+++ data_generation.py
@@ -1,6 +1,7 @@
 import pandas as pd
 from faker import Faker
 import random
+import numpy as np
 
 # Load the original Excel file
 input_file = './data/DummyData.xlsx'
@@ -16,12 +17,16 @@ data_types = {}
 for column in df.columns:
     if df[column].dtype == 'object':
         data_types[column] = 'string'
+        print("String", column)
     elif df[column].dtype == 'int64':
         data_types[column] = 'integer'
+        print("int64",column)
     elif df[column].dtype == 'float64':
         data_types[column] = 'float'
-    elif df[column].dtype == 'datetime64':
+        print("float64",column)
+    else:
         data_types[column] = 'date'  # Identify datetime columns
+        print("date",column)
 
 # Create a list to store the new data
 new_data_rows = []
@@ -47,14 +52,21 @@ for _ in range(num_new_rows):
             max_val = df[column].max()
             new_row[column] = random.uniform(min_val, max_val)
         elif data_type == 'date':
-            # For datetime columns, generate a random date within a specified range
-            start_date = df[column].min()
-            end_date = df[column].max()
-            new_row[column] = fake.date_between(start_date=start_date, end_date=end_date)
+            new_row[column] = fake.date_between()
+             
     new_data_rows.append(new_row)
 
 # Convert the list of new data rows to a DataFrame
 result_df = pd.DataFrame(new_data_rows, columns=df.columns)
+
+# Specify the percentage of rows to randomly drop
+drop_percentage = 80  # You can change this as needed
+
+# Randomly drop values in the "Date of Thrombosis" column
+if "Date of Thrombosis" in result_df.columns:
+    num_rows_to_drop = int(len(result_df) * (drop_percentage / 100))
+    rows_to_drop = random.sample(range(len(result_df)), num_rows_to_drop)
+    result_df.loc[rows_to_drop, "Date of Thrombosis"] = np.nan
 
 # Save the combined data to a new Excel file
 output_file = './data/DummyData_Extended.xlsx'
